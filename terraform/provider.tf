3db213be23ca8209f4493c232eba380f9e893a22
Manav Nim <l00171238@atu.ie>
2024-02-21 12:05:39 +0000

    depricated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,6 @@ terraform {
     google = {
       source  = "hashicorp/google"
       version = "~> 3.52.0"
-      credentials = var.service_account_key
     }
   }
   required_version = ">= 0.13"
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,6 +1,2 @@
 project_id = "heroic-psyche-414901"
 region     = "europe-west1"
-
-variable "service_account_key" {
-    sensivite = true
-}
