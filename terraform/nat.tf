80ce6f282fb44a0b75a30fc8dc26941dbb7b875e
Manav Nim <l00171238@atu.ie>
2024-02-21 13:38:16 +0000

    Fix: type in nat

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -6,6 +6,22 @@ resource "google_compute_router_nat" "nat" {
 
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 
+
+// define the NAT configuration specifying the subnet to NAT
   subnetwork {
-    name = google_compute_subnetwork.private.id }
+    name = google_compute_subnetwork.private.id 
+    source_ip_ranges_to_nat = "ALL_IP_RANGES"
+    }
+
+nat_ip {
+    name = google_compute_address.nat.self.link
+    }
+
+resource "google_compute_address" "nat" {
+  name = "nat"
+  region = var.region
+  address_type = "EXTERNAL"
+  depends_on = [google_compute_router_nat.nat]
+
+}
 }
