abbe95b3b2dfb6d8f040e6bfb07b0fe2a6f95816
Manav Nim <l00171238@atu.ie>
2024-02-21 11:39:40 +0000

    provision GKE

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,6 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-        credentials = base64decode(var.service_account_key)
         project     = var.project_id
         region      = var.region
       version = "~> 3.52.0"
