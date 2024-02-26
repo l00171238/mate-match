26c939ed518cfc885ed427c473a77dbc561a8aab
Manav Nim <l00171238@atu.ie>
2024-02-26 21:02:49 +0000

    Fix : change permission

--- terraform/9-service-account.tf
+++ terraform/9-service-account.tf
@@ -1,7 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_service_account
 resource "google_service_account" "terraform" {
   account_id = "terraform"
-
+}
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project_iam
 resource "google_project_iam_member" "terraform" {
   project = "heroic-psyche-414901"
