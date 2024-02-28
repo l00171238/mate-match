3b568bc953874a2e6a1591eafdbe527fcdb49a87
Manav Nim <l00171238@atu.ie>
2024-02-28 16:17:58 +0000

    fix: added credent

--- terraform/1-provider.tf
+++ terraform/1-provider.tf
@@ -1,8 +1,12 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 provider "google" {
+  credentials = file("./heroic-psyche-414901-2d224203d9c2.json")
   project = "heroic-psyche-414901"
   region  = "europe-west1"
 }
+
+
+
 
 # https://www.terraform.io/language/settings/backends/gcs
 terraform {
