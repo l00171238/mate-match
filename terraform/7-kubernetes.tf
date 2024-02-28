22e56bcecb7aac3abaa84480e216dda3e2d10130
Manav Nim <l00171238@atu.ie>
2024-02-28 16:07:04 +0000

    update: Jenkins integrated

--- terraform/7-kubernetes.tf
+++ terraform/7-kubernetes.tf
@@ -44,10 +44,10 @@ resource "google_container_cluster" "primary" {
   }
 
   #   Jenkins use case
-  #   master_authorized_networks_config {
-  #     cidr_blocks {
-  #       cidr_block   = "10.0.0.0/18"
-  #       display_name = "private-subnet-w-jenkins"
-  #     }
-  #   }
+    master_authorized_networks_config {
+      cidr_blocks {
+        cidr_block   = "10.0.0.0/18"
+        display_name = "private-subnet-w-jenkins"
+      }
+    }
 }
